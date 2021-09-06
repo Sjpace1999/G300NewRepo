514a8cf58b76088e02323590eeee523ad43eca4e
Sjpace1999 <sjpace1999@gmail.com>
2021-09-06 13:20:23 -0400

    Revert "All geometry placed"
    This reverts commit a3945f1c55bab1a8d5e7f1683d33071db3c7bb7f.

--- LevelBlockingRemake/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ LevelBlockingRemake/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -16,9 +16,9 @@ namespace StarterAssets
 	{
 		[Header("Player")]
 		[Tooltip("Move speed of the character in m/s")]
-		public float MoveSpeed = 10.0f;
+		public float MoveSpeed = 2.0f;
 		[Tooltip("Sprint speed of the character in m/s")]
-		public float SprintSpeed = 20.0f;
+		public float SprintSpeed = 5.335f;
 		[Tooltip("How fast the character turns to face movement direction")]
 		[Range(0.0f, 0.3f)]
 		public float RotationSmoothTime = 0.12f;
